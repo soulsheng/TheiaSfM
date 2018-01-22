66576048267b5d4ec43e1be41726e15717fdc578
s <s>
2018-01-22 11:32:37 +0800

    add error code - -47

--- build12/common/LaunchPMVS2.cpp
+++ build12/common/LaunchPMVS2.cpp
@@ -1,6 +1,7 @@
 
 #include "LaunchPMVS2.h"
 #include "utility_common.h"
+#include <theia/io/read_ply_file.h>
 
 int lanch_external_bin(std::string& bin, std::string& parameter, std::string& path, int nShowType)
 {
@@ -74,6 +75,28 @@ int run_pmvs(std::string& pmvsPath, int threshold_group)
 	std::ostringstream parameter;
 	parameter << pmvsPath << " option-0000" << " thresholdGroup " << threshold_group;
 	
-	return lanch_external_bin(std::string("pmvs2.exe"), parameter.str(), exePath);
+	nRetCode = lanch_external_bin(std::string("pmvs2.exe"), parameter.str(), exePath);
+
+
+	theia::Vector3dVec world_points;
+	theia::Vector3fVec point_normals;
+	theia::Vector3fVec point_colors;
+
+	std::string str_ply_file = pmvsPath + "models\\option-0000.ply";
+	if (!theia::ReadPlyFile(str_ply_file, world_points, point_normals, point_colors))
+		printf("can not open ply file!\n");
+
+	LOG(INFO) << "稠密重建三维点的数目为：" << world_points.size();
+
+	if (0 == world_points.size())
+	{
+		nRetCode = -47;
+
+		LOG(INFO) << "异常返回！异常代码：" << nRetCode << std::endl
+			<< "异常描述：稀疏点全部滤除，剩余0个三维点；可能原因：图片数目过少；"
+			<< "建议措施：增加图片数目";
+	}
+
+	return nRetCode;
 
 }
